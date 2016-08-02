8c5e84e906fd3381eb85e6d167b7f2a47a021fcf
Lisa Jones <ljones4521@yahoo.com>
2016-08-01 21:04:13 -0400

    added comment view

--- db/migrate/20160731232235_create_comments.rb
+++ db/migrate/20160731232235_create_comments.rb
@@ -1,12 +1,13 @@
-  class CreateComments < ActiveRecord::Migration
-    def change
-      create_table :comments do |t|
-        t.integer :link_id
-        t.text :body
-        t.references :user, index: true
 
-        t.timestamps
+  class CreateComments < ActiveRecord::Migration
+      def change
+        create_table :comments do |t|
+          t.integer :link_id
+          t.text :body
+          t.references :user, index: true, foreign_key: true
+
+          t.timestamps null: false
+        end
+        add_index :comments, :link_id
       end
-      add_index :comments, :link_id
     end
-  end
